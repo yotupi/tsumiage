9aabf3d3a4541cd99560b8fba3516125ce539c67
yotupi <321041858+yotupi@users.noreply.github.com>
2026-09-06 21:59:55 +0900

    Add calendar journal delete

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="tsumiage-v1-1";
+const CACHE="tsumiage-v1-2";
 const ASSETS=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
 
 self.addEventListener("install",e=>{
